48102f45df8bd74b005148ca40cab68e8006f440
mark.molenmaker <mark.molenmaker@technolution.nl>
2023-07-10 10:23:54 +0200

    Refactored the chat

--- src/views/container/ContainerChat.vue
+++ src/views/container/ContainerChat.vue
@@ -3,13 +3,12 @@ import {useRoute} from "vue-router";
 
 export default {
   name: 'ContainerChat',
-  computed: { url () {
-    const route = useRoute()
-    const fontSize = route.query.fontSize ? route.query.fontSize : 16
-    return `${process.env.VUE_APP_BOTRIX_CHAT_URL}&bots=true&emojis=true&hideCommands=true&hideMessages=false
-      &hideMessagesSeconds=10&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent
-      &fontSize=${fontSize}`
-  } },
+  computed: {
+    url () {
+      const route = useRoute()
+      const fontSize = route.query.fontSize || 16
+      return `${process.env.VUE_APP_BOTRIX_CHAT_URL}&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=${fontSize}&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent` },
+  },
 }
 </script>
 
