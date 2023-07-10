2f588618859ae1e986119023aaf20fb8176cc395
mark.molenmaker <mark.molenmaker@technolution.nl>
2023-07-10 10:55:47 +0200

    Altered the fontSize usage

--- src/views/container/ContainerChat.vue
+++ src/views/container/ContainerChat.vue
@@ -3,18 +3,35 @@ import {useRoute} from "vue-router";
 
 export default {
   name: 'ContainerChat',
-  computed: {
-    url () {
-      const route = useRoute()
-      const fontSize = route.query.fontSize || 16
-      return `${process.env.VUE_APP_BOTRIX_CHAT_URL}&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=${fontSize}&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent` },
-  },
+  data () { return { loaded: false, src: null } },
+  mounted () {
+    const route = useRoute()
+    const fontSize = route.query.fontSize
+    switch (fontSize) {
+      case 'large':
+        this.src = 'https://botrix.live/widgets/chat/?bid=2QA6EfEf2MUVSCPH2kd%2FBw&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=22&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent'
+        break
+      case 'normal':
+        this.src = 'https://botrix.live/widgets/chat/?bid=2QA6EfEf2MUVSCPH2kd%2FBw&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=16&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent'
+        break
+      case 'medium':
+        this.src = 'https://botrix.live/widgets/chat/?bid=2QA6EfEf2MUVSCPH2kd%2FBw&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=14&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent'
+        break
+      case 'small':
+        this.src = 'https://botrix.live/widgets/chat/?bid=2QA6EfEf2MUVSCPH2kd%2FBw&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=12&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent'
+        break
+      default:
+        this.src = 'https://botrix.live/widgets/chat/?bid=2QA6EfEf2MUVSCPH2kd%2FBw&bots=true&emojis=true&hideCommands=true&hideMessages=false&hideMessagesSeconds=10&fontSize=16&trovo=false&twitch=false&youtube=false&kick=true&platformIcon=false&theme=transparent'
+        break
+    }
+    this.loaded = true
+  }
 }
 </script>
 
 <template>
   <div class="bg bg-blue tp-1 bt-1">
-    <iframe :src="this.url"></iframe>
+    <iframe v-if="loaded" :src="this.src"></iframe>
   </div>
 </template>
 
