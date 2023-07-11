d6a70a575f469c920c5a4f0c5468075c6d52b923
inazumark <inazumarkmail@gmail.com>
2023-07-11 20:41:25 +0200

    Automatically snap the selected slot in the middle

--- src/components/BonusList.vue
+++ src/components/BonusList.vue
@@ -8,10 +8,18 @@ export default{
     showActive: { type: Boolean, default: false },
     showPayout: { type: Boolean, default: false }
   },
+  methods: {
+    multi (bonus) {
+      if (bonus.payout === null || bonus.payout === undefined) return '-'
+      const payout = Number(bonus.payout.substring(2, bonus.payout.length - 1))
+      const bet_size = Number(bonus.bet_size.substring(2, bonus.bet_size.length - 1))
+      return (payout / bet_size).toFixed(2)
+    }
+  },
   computed: {
     ...mapGetters(['bonus_count', 'bonus', 'is_opening', 'slot_selected']),
     gridTemplateColumns: function () {
-      return this.showPayout ? '40px 1fr 70px 70px' : '40px 1fr 70px'
+      return this.showPayout ? '40px 1fr 70px 70px 70px' : '40px 1fr 70px'
     }
   },
   // watch: {
@@ -25,41 +33,35 @@ export default{
       // If the wrapper is the same size as the content, we don't need to scroll
       if (wrapper.scrollHeight === wrapper.clientHeight) return
 
-      // Check if we need to scroll to the selected bonus
+      // Scroll to selected row in the middle, when opening
       if (this.is_opening) {
-
-        // Check if we are in the correct url
         if (this.$route.path === '/gambling/widgets/bonus_opening') {
-
-          // Get the position of the selected bonus
           const index = this.slot_selected
-
-          // Get the amount of rows in the wrapper
           const rows = wrapper.children.length
-
-          // Get the height of a single row
           const rowHeight = wrapper.scrollHeight / rows
 
-          // Calculate the position of the selected bonus
-          const position = rowHeight * (index - 1) - 1
+          // Get the amount of rows that fit in the visible area
+          const visibleRows = Math.floor(wrapper.clientHeight / rowHeight)
+          const offsetRows = Math.floor(visibleRows / 2)
+
+          const position = rowHeight * (index - 1) - offsetRows * rowHeight - 1
 
           // If the selected bonus is already on top, we don't need to scroll
           if (wrapper.scrollTop === position) return
-
-          // Scroll to the selected bonus
           wrapper.scrollTo({top: position, behavior: 'smooth'})
-
+          return
         }
       }
-        // Scroll down 1px
-        wrapper.scrollBy({top: 1, behavior: 'auto'})
 
-        // If we've reached the bottom, move the first child to the end, so we can keep scrolling
-        if (Math.floor(wrapper.scrollHeight - wrapper.scrollTop) <= wrapper.clientHeight) {
-          const fragment = document.createDocumentFragment()
-          fragment.appendChild(wrapper.firstElementChild)
-          wrapper.appendChild(fragment)
-        }
+      // Scroll down 1px
+      wrapper.scrollBy({top: 1, behavior: 'auto'})
+
+      // If we've reached the bottom, move the first child to the end, so we can keep scrolling
+      if (Math.floor(wrapper.scrollHeight - wrapper.scrollTop) <= wrapper.clientHeight) {
+        const fragment = document.createDocumentFragment()
+        fragment.appendChild(wrapper.firstElementChild)
+        wrapper.appendChild(fragment)
+      }
 
     }, 50)
   },
@@ -70,7 +72,7 @@ export default{
 <template>
     <table class="bonus-table">
       <tr class="bonus" :style="{ gridTemplateColumns: this.gridTemplateColumns }">
-        <th>#</th><th class="slot">Slot</th><th>Bet</th><th v-if="this.showPayout">Payout</th>
+        <th>#</th><th class="slot">Slot</th><th>Bet</th><th>Multi</th><th v-if="this.showPayout">Payout</th>
       </tr>
       <tbody class="wrapper" ref="wrapper">
         <tr class="bonus" :class="{active: bonus(index).active && this.showActive}"
@@ -79,6 +81,7 @@ export default{
           <td>{{ index }}</td>
           <td class="slot">{{ bonus(index).name }}</td>
           <td>{{ bonus(index).bet_size }}</td>
+          <td v-if="this.showPayout">{{ multi(bonus) }}</td>
           <td v-if="this.showPayout">{{ bonus(index).payout }}</td>
         </tr>
       </tbody>
